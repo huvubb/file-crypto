793bfe9ae67631b4b0326e5434c16e28c91120e1
邓涛 <810372789@qq.com>
2026-07-24 21:01:25 +0800

    v2.3: MakeAdminShortcut use pure COM, no PowerShell

--- src/make_shortcut.cpp
+++ src/make_shortcut.cpp
@@ -1,28 +1,48 @@
 #include <windows.h>
 #include <cstdio>
 #include <shlobj.h>
+#include <shobjidl.h>
+
 #pragma comment(linker, "/SUBSYSTEM:WINDOWS")
 #pragma comment(linker, "/ENTRY:mainCRTStartup")
 
-int main() {
-    // 桌面上建个快捷方式，指向 .com，带管理员标志
-    wchar_t ps[2048];
-    swprintf(ps, 2048,
-        L"powershell -WindowStyle Hidden -Command "
-        L"$s=(New-Object -ComObject WScript.Shell).CreateShortcut("
-        L"'D:\\Desktop\\被报毒用管理员运行我.lnk');"
-        L"$s.TargetPath='D:\\Desktop\\被报毒用管理员运行我.com';"
-        L"$s.Save();"
-        L"$b=[IO.File]::ReadAllBytes('D:\\Desktop\\被报毒用管理员运行我.lnk');"
-        L"$b[0x15]=$b[0x15]-bor0x20;"
-        L"[IO.File]::WriteAllBytes('D:\\Desktop\\被报毒用管理员运行我.lnk',$b)");
-    
-    PROCESS_INFORMATION pi;
-    STARTUPINFOW si = {sizeof(si)};
-    CreateProcessW(NULL, ps, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi);
-    WaitForSingleObject(pi.hProcess, 8000);
-    CloseHandle(pi.hProcess); CloseHandle(pi.hThread);
+#ifndef SLDF_RUNAS_USER
+#define SLDF_RUNAS_USER 0x00000020
+#endif
 
-    MessageBoxW(NULL, L"桌面已创建快捷方式\n双击它就会弹 UAC 申请管理员权限\n（原文件未修改）", L"搞定", MB_OK);
+int main() {
+    CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+
+    IShellLinkW* psl = NULL;
+    HRESULT hr = CoCreateInstance(CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER,
+                                  IID_IShellLinkW, (void**)&psl);
+    if (FAILED(hr) || !psl) {
+        MessageBoxW(NULL, L"创建失败", L"错误", MB_ICONERROR);
+        CoUninitialize(); return 1;
+    }
+
+    psl->SetPath(L"D:\\Desktop\\被报毒用管理员运行我.com");
+
+    // 设置管理员运行标志
+    IShellLinkDataList* pdl = NULL;
+    hr = psl->QueryInterface(IID_IShellLinkDataList, (void**)&pdl);
+    if (SUCCEEDED(hr) && pdl) {
+        DWORD flags = 0;
+        pdl->GetFlags(&flags);
+        flags |= SLDF_RUNAS_USER;
+        pdl->SetFlags(flags);
+        pdl->Release();
+    }
+
+    IPersistFile* ppf = NULL;
+    hr = psl->QueryInterface(IID_IPersistFile, (void**)&ppf);
+    if (SUCCEEDED(hr) && ppf) {
+        ppf->Save(L"D:\\Desktop\\被报毒用管理员运行我.lnk", TRUE);
+        ppf->Release();
+    }
+
+    psl->Release();
+    CoUninitialize();
+    MessageBoxW(NULL, L"桌面快捷方式已创建\n双击即弹UAC", L"成功", MB_OK);
     return 0;
 }
